24ef0245011d8511cfac8899411b4fd079e44ffa
brilianM <121849622+brilianM@users.noreply.github.com>
2023-01-03 14:42:46 +0700

    Add files via upload

---
+++ soal1.py
@@ -0,0 +1,55 @@
+print("========================================")
+print("*************Justice League*************")
+username = str(input("masukan username anda: "))
+if username == ("brucewyne"):
+    print("=====WELCOME",username,"=====")
+    Tanggota = print("1. Tambah Anggota Justice League")
+    Hanggota = print("2. Hapus Anggota Justice League")
+    Panggota = print("3. Tampilkan Anggota Justice League")
+    e = print("4. Exit")
+    pilihan = str(input("masukan pilihan anda anda: "))
+    if pilihan == ("1"):
+        baru = str(input("Nama Anggota baru: "))
+        print("data",baru,"berhasilditambahkan")
+    elif pilihan ==("2"):
+            str(input("Anggota yang akan dihapus: "))
+    elif pilihan ==("3"):
+            print("Daftar anggota Justice League: ")
+    elif pilihan ==("4"):
+            print("See you next time MR",username)
+elif username == ("victorstone"):
+    print("=====WELCOME",username,"=====")
+    Tanggota = print("1. Tambah Anggota Justice League")
+    Hanggota = print("2. Hapus Anggota Justice League")
+    Panggota = print("3. Tampilkan Anggota Justice League")
+    e = print("4. Exit")
+    pilihan = str(input("masukan pilihan anda anda: "))
+    if pilihan == ("1"):
+        baru = str(input("Nama Anggota baru: "))
+        print("data",baru,"berhasilditambahkan")
+    elif pilihan ==("2"):
+            str(input("Anggota yang akan dihapus: "))
+    elif pilihan ==("3"):
+            print("Daftar anggota Justice League: ")
+    elif pilihan ==("4"):
+            print("See you next time MR",username)
+elif username == ("ciscoramon"):
+    print("=====WELCOME",username,"=====")
+    Tanggota = print("1. Tambah Anggota Justice League")
+    Hanggota = print("2. Hapus Anggota Justice League")
+    Panggota = print("3. Tampilkan Anggota Justice League")
+    e = print("4. Exit")
+    pilihan = str(input("masukan pilihan anda anda: "))
+    if pilihan == ("1"):
+        baru = str(input("Nama Anggota baru: "))
+        print("data",baru,"berhasilditambahkan")
+    elif pilihan ==("2"):
+            str(input("Anggota yang akan dihapus: "))
+    elif pilihan ==("3"):
+            print("Daftar anggota Justice League: ")
+    elif pilihan ==("4"):
+            print("See you next time MR",username)
+else :
+    print("Access Denied")
+        
+    
